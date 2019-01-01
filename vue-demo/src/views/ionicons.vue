8d3bd8704b5195595fcb563814f3dc4a6b86d638
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-01 23:44:27 +0800

    Use a different name for the second v-for

--- vue-demo/src/views/ionicons.vue
+++ vue-demo/src/views/ionicons.vue
@@ -1,27 +1,29 @@
 <template>
-<div>
-  <h1>Ionicons Demo</h1>
+  <div>
+    <h1>Ionicons Demo</h1>
 
-  <div class="grid">
-    <table v-for="(item, index) in ionicons"
-      v-bind:key="index">
-      <thead>
-        <tr>
-          <th>{{ item.name }}</th>
-          <th>Icon</th>
-        </tr>
-      </thead>
-      
-      <tbody>
-        <tr v-for="(item, index) in item.icons"
-          v-bind:key="index">
-          <td>{{ item }}</td>
-          <td><ion-icon v-bind:name="item"/></td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="grid">
+      <table
+        v-for="(item) in ionicons"
+        v-bind:key="item.name">
+        <thead>
+          <tr>
+            <th>{{ item.name }}</th>
+            <th>Icon</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          <tr
+            v-for="(subitem, index) in item.icons"
+            v-bind:key="index">
+            <td>{{ subitem }}</td>
+            <td><ion-icon v-bind:name="subitem" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -48,11 +50,11 @@ export default {
 <style lang="scss" scoped>
 .grid {
   display: grid;
-  grid-row-gap: 2rem;
+  row-gap: 2rem;
 
   @media (min-width: 768px) {
     grid-template-columns: 1fr 1fr;
-    grid-column-gap: 2.5rem;
+    column-gap: 2.5rem;
   }
 }
 
